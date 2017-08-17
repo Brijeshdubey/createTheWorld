7a13bf751003add59838d7f5796bd4e26e243d86
Brijesh Dubey <brijeshdubey@Brijeshs-Mac-mini.local>
2017-08-17 18:23:30 +0530

    Initial Commit

---
+++ createTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  createTheWorld
+//
+//  Created by Brijesh Dubey on 17/08/17.
+//  Copyright © 2017 Brijesh Dubey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
